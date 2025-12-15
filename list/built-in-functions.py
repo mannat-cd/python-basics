b938a3f3fbb5523da0c19331645ea5663515da58
Mannat <mannat.garglic@gmail.com>
2025-12-15 20:06:19 +0530

    Create built-in-functions.py

---
+++ list/built-in-functions.py
@@ -0,0 +1,32 @@
+mylist = [10, 20, 30]
+
+mylist.append(40)
+print(mylist)
+
+mylist.reverse()
+print(mylist)
+
+mylist.insert(1, 50)
+print(mylist)
+
+mylist.extend([4, 5])
+print(mylist)
+
+mylist.pop(1)
+print(mylist)
+
+mylist.remove(10)
+print(mylist)
+
+mylist.sort()
+print(mylist)
+
+print(max(mylist))
+
+print(mylist.index(3))
+
+print(mylist.count(30))
+
+print(list("hello"))
+
+print(len([10, 20, 30]))
